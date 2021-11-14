02ce827673bd4b677a5c1c227b0a730d3b320f83
nakaji <0619nakaji@gmail.com>
2021-11-14 21:57:51 +0900

    終了

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; //LoadSceneを使うため
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class PlayerController : MonoBehaviour
     float jumpForce = 680.0f;
     float walkForce = 30.0f;
     float maxWalkSpeed = 2.0f;
+    float threshold = 0.2f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,15 +23,15 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // ジャンプする
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0) && this.rigid2D.velocity.y == 0)
         {
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
 
         // 左右移動
         int key = 0;
-        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
-        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+        if (Input.acceleration.x > this.threshold) key = 1;
+        if (Input.acceleration.x < -this.threshold) key = -1;
 
         // プレイヤの速度
         float speedx = Mathf.Abs(this.rigid2D.velocity.x);
@@ -48,11 +50,18 @@ public class PlayerController : MonoBehaviour
 
         // プレイヤの速度に応じてアニメーション速度を変える
         this.animator.speed = speedx / 2.0f;
+
+        //画面外に出た場合は最初から
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
     }
 
     // ゴールに到着
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("ゴール");
+        SceneManager.LoadScene("ClearScene");
     }
 }
